ba0051de071a4d0151eb9c7cff8f4c5e805f30b2
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-12-13 17:47:30 -0500

    commit tree 1b15fff9a6d7060cd386424e85f7982759bbb201

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+    attr_reader :a, :b, :c
+  
+    def initialize(a, b, c)
+      @a = a
+      @b = b
+      @c = c 
+    end
+
+    def kind 
+      validate_triangle
+      if a == b && b == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+    def triangle_no_size?
+      [a, b, c].all?(&:positive?)
+    end
+
+    def triangle_negative_sides?
+      a + b > c && a + c > b && b + c > a
+    end
+
+    def validate_triangle
+      raise TriangleError unless triangle_no_size? && triangle_negative_sides?
+    end
+
+    class TriangleError < StandardError
+    end
 end
+
